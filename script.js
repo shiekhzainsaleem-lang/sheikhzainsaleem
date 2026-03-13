6316e36dfa711696ef3fde05ad165b7f0fcb9b4d
shiekhzainsaleem-lang <shiekhzainsaleem@gmail.com>
2026-03-14 01:32:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80
+    },
+    "color": {
+      "value": "#38bdf8"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5
+    },
+    "size": {
+      "value": 3
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#38bdf8",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  }
+});
